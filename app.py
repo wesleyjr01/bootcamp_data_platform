7bda8b39c784f870198623592f3efa82e26dc4c8
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-13 13:24:47 -0300

    cria DMS

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from aws_cdk import core as cdk
 from bootcamp_data_platform.data_lake.stack import DataLakeStack
 from bootcamp_data_platform.common_stack import CommonStack
+from bootcamp_data_platform.dms.stack import DmsStack
 
 app = cdk.App()
 data_lake = DataLakeStack(app)
 common_stack = CommonStack(app)
+dms = DmsStack(
+    app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
+)
 
 app.synth()
