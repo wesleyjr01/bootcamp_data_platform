eb84c45ac6f9827b9f57ab6dbe3516581a35c5d8
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-13 15:52:26 -0300

    adding all stacks, except dms

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from bootcamp_data_platform.dms.stack import DmsStack
 app = cdk.App()
 data_lake = DataLakeStack(app)
 common_stack = CommonStack(app)
-dms = DmsStack(
-    app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
-)
+# dms = DmsStack(
+#     app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
+# )
 
 app.synth()
