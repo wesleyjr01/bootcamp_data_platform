fd77bf5c0edd2c6a5a2d6cb8777131e0f1c64a6b
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-13 13:04:58 -0300

    creating commong_stack, which creates RDS + VPC

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from aws_cdk import core as cdk
 from bootcamp_data_platform.data_lake.stack import DataLakeStack
+from bootcamp_data_platform.common_stack import CommonStack
 
 app = cdk.App()
 data_lake = DataLakeStack(app)
+common_stack = CommonStack(app)
 
 app.synth()
