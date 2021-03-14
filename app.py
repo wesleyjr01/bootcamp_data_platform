6494c329e22a18393fa9dda20d2c1e31e3a28e12
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-14 16:15:36 -0300

    build databricks stack

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from aws_cdk import core as cdk
+import databricks
 from bootcamp_data_platform.data_lake.stack import DataLakeStack
 from bootcamp_data_platform.common_stack import CommonStack
 from bootcamp_data_platform.dms.stack import DmsStack
@@ -6,6 +7,7 @@ from bootcamp_data_platform.kinesis.stack import KinesisStack
 from bootcamp_data_platform.glue_catalog.stack import GlueCatalogStack
 from bootcamp_data_platform.athena.stack import AthenaStack
 from bootcamp_data_platform.redshift.stack import RedshiftStack
+from bootcamp_data_platform.databricks.stack import DatabricksStack
 
 app = cdk.App()
 data_lake = DataLakeStack(app)
@@ -26,4 +28,5 @@ redshift = RedshiftStack(
     data_lake_processed=data_lake.data_lake_processed_bucket,
     common_stack=common_stack,
 )
+databricks - DatabricksStack(app)
 app.synth()
