144babad628bb2ac08b81e5de5fb5e7d591d8759
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-14 14:31:11 -0300

    adiciona athena stack

--- app.py
+++ app.py
@@ -4,18 +4,20 @@ from bootcamp_data_platform.common_stack import CommonStack
 from bootcamp_data_platform.dms.stack import DmsStack
 from bootcamp_data_platform.kinesis.stack import KinesisStack
 from bootcamp_data_platform.glue_catalog.stack import GlueCatalogStack
+from bootcamp_data_platform.athena.stack import AthenaStack
 
 app = cdk.App()
 data_lake = DataLakeStack(app)
-common_stack = CommonStack(app)
-dms = DmsStack(
-    app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
-)
+# common_stack = CommonStack(app)
+# dms = DmsStack(
+#     app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
+# )
 kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(
     app,
     raw_data_lake_bucket=data_lake.data_lake_raw_bucket,
     processed_data_lake_bucket=data_lake.data_lake_processed_bucket,
 )
+athena = AthenaStack(app)
 
 app.synth()
