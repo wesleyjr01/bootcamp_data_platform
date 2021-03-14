e2aa8f3e18191ca8a3934a98a6cfbc7720f9358e
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-14 15:49:43 -0300

    adding redshift, re-up common_stack rds

--- app.py
+++ app.py
@@ -5,19 +5,25 @@ from bootcamp_data_platform.dms.stack import DmsStack
 from bootcamp_data_platform.kinesis.stack import KinesisStack
 from bootcamp_data_platform.glue_catalog.stack import GlueCatalogStack
 from bootcamp_data_platform.athena.stack import AthenaStack
+from bootcamp_data_platform.redshift.stack import RedshiftStack
 
 app = cdk.App()
 data_lake = DataLakeStack(app)
-# common_stack = CommonStack(app)
+common_stack = CommonStack(app)
 # dms = DmsStack(
 #     app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
 # )
-kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+# kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(
     app,
     raw_data_lake_bucket=data_lake.data_lake_raw_bucket,
     processed_data_lake_bucket=data_lake.data_lake_processed_bucket,
 )
 athena = AthenaStack(app)
-
+redshift = RedshiftStack(
+    app,
+    data_lake_raw=data_lake.data_lake_raw_bucket,
+    data_lake_processed=data_lake.data_lake_processed_bucket,
+    common_stack=common_stack,
+)
 app.synth()
