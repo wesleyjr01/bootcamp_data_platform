b070edd2a33e878a5c338a077f136c2194c2635f
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-14 11:42:53 -0300

    building kinesis stack

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from aws_cdk import core as cdk
 from bootcamp_data_platform.data_lake.stack import DataLakeStack
 from bootcamp_data_platform.common_stack import CommonStack
 from bootcamp_data_platform.dms.stack import DmsStack
+from bootcamp_data_platform.kinesis.stack import KinesisStack
 
 app = cdk.App()
 data_lake = DataLakeStack(app)
@@ -9,5 +10,6 @@ common_stack = CommonStack(app)
 dms = DmsStack(
     app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
 )
+kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 
 app.synth()
